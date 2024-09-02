09f1be3d03ff758e2e3d3a6b67bb4db4c20eca68
Reda <155382825+0xKa@users.noreply.github.com>
2024-09-02 20:51:48 +0400

    Add files via upload

---
+++ StudentManagementSystem/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace StudentManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtMajor = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtSearchByID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listView_Students = new System.Windows.Forms.ListView();
+            this.cID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cAge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.aMajor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gbView = new System.Windows.Forms.GroupBox();
+            this.rbDetails = new System.Windows.Forms.RadioButton();
+            this.rbLargeIcon = new System.Windows.Forms.RadioButton();
+            this.rbSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rbList = new System.Windows.Forms.RadioButton();
+            this.rbTile = new System.Windows.Forms.RadioButton();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.gbView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(6, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Major";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label3.Location = new System.Drawing.Point(6, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label4.Location = new System.Drawing.Point(6, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Name";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtStudentID.Location = new System.Drawing.Point(134, 19);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(186, 26);
+            this.txtStudentID.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtName.Location = new System.Drawing.Point(134, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(186, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtAge.Location = new System.Drawing.Point(134, 88);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(186, 26);
+            this.txtAge.TabIndex = 6;
+            // 
+            // txtMajor
+            // 
+            this.txtMajor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMajor.Location = new System.Drawing.Point(134, 121);
+            this.txtMajor.Name = "txtMajor";
+            this.txtMajor.Size = new System.Drawing.Size(186, 26);
+            this.txtMajor.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(372, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 41);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(453, 19);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 41);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(372, 66);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 41);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(453, 150);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(453, 66);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 41);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtSearchByID
+            // 
+            this.txtSearchByID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtSearchByID.Location = new System.Drawing.Point(453, 121);
+            this.txtSearchByID.Name = "txtSearchByID";
+            this.txtSearchByID.Size = new System.Drawing.Size(75, 23);
+            this.txtSearchByID.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(342, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 23);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Search By ID:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // listView_Students
+            // 
+            this.listView_Students.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cID,
+            this.cName,
+            this.cAge,
+            this.aMajor});
+            this.listView_Students.FullRowSelect = true;
+            this.listView_Students.LargeImageList = this.imageList2;
+            this.listView_Students.Location = new System.Drawing.Point(28, 176);
+            this.listView_Students.Name = "listView_Students";
+            this.listView_Students.Size = new System.Drawing.Size(346, 225);
+            this.listView_Students.SmallImageList = this.imageList1;
+            this.listView_Students.TabIndex = 17;
+            this.listView_Students.UseCompatibleStateImageBehavior = false;
+            this.listView_Students.View = System.Windows.Forms.View.Details;
+            // 
+            // cID
+            // 
+            this.cID.Text = "ID";
+            // 
+            // cName
+            // 
+            this.cName.Text = "Name";
+            this.cName.Width = 100;
+            // 
+            // cAge
+            // 
+            this.cAge.Text = "Age";
+            // 
+            // aMajor
+            // 
+            this.aMajor.Text = "Major";
+            this.aMajor.Width = 120;
+            // 
+            // gbView
+            // 
+            this.gbView.Controls.Add(this.rbTile);
+            this.gbView.Controls.Add(this.rbList);
+            this.gbView.Controls.Add(this.rbSmallIcon);
+            this.gbView.Controls.Add(this.rbLargeIcon);
+            this.gbView.Controls.Add(this.rbDetails);
+            this.gbView.Location = new System.Drawing.Point(393, 176);
+            this.gbView.Name = "gbView";
+            this.gbView.Size = new System.Drawing.Size(162, 225);
+            this.gbView.TabIndex = 18;
+            this.gbView.TabStop = false;
+            this.gbView.Text = "View";
+            // 
+            // rbDetails
+            // 
+            this.rbDetails.AutoSize = true;
+            this.rbDetails.Checked = true;
+            this.rbDetails.Location = new System.Drawing.Point(41, 34);
+            this.rbDetails.Name = "rbDetails";
+            this.rbDetails.Size = new System.Drawing.Size(57, 17);
+            this.rbDetails.TabIndex = 0;
+            this.rbDetails.TabStop = true;
+            this.rbDetails.Text = "Details";
+            this.rbDetails.UseVisualStyleBackColor = true;
+            this.rbDetails.CheckedChanged += new System.EventHandler(this.rbDetails_CheckedChanged);
+            // 
+            // rbLargeIcon
+            // 
+            this.rbLargeIcon.AutoSize = true;
+            this.rbLargeIcon.Location = new System.Drawing.Point(41, 68);
+            this.rbLargeIcon.Name = "rbLargeIcon";
+            this.rbLargeIcon.Size = new System.Drawing.Size(76, 17);
+            this.rbLargeIcon.TabIndex = 1;
+            this.rbLargeIcon.Text = "Large Icon";
+            this.rbLargeIcon.UseVisualStyleBackColor = true;
+            this.rbLargeIcon.CheckedChanged += new System.EventHandler(this.rbLargeIcon_CheckedChanged);
+            // 
+            // rbSmallIcon
+            // 
+            this.rbSmallIcon.AutoSize = true;
+            this.rbSmallIcon.Location = new System.Drawing.Point(41, 102);
+            this.rbSmallIcon.Name = "rbSmallIcon";
+            this.rbSmallIcon.Size = new System.Drawing.Size(74, 17);
+            this.rbSmallIcon.TabIndex = 2;
+            this.rbSmallIcon.Text = "Small Icon";
+            this.rbSmallIcon.UseVisualStyleBackColor = true;
+            this.rbSmallIcon.CheckedChanged += new System.EventHandler(this.rbSmallIcon_CheckedChanged);
+            // 
+            // rbList
+            // 
+            this.rbList.AutoSize = true;
+            this.rbList.Location = new System.Drawing.Point(41, 136);
+            this.rbList.Name = "rbList";
+            this.rbList.Size = new System.Drawing.Size(41, 17);
+            this.rbList.TabIndex = 3;
+            this.rbList.Text = "List";
+            this.rbList.UseVisualStyleBackColor = true;
+            this.rbList.CheckedChanged += new System.EventHandler(this.rbList_CheckedChanged);
+            // 
+            // rbTile
+            // 
+            this.rbTile.AutoSize = true;
+            this.rbTile.Location = new System.Drawing.Point(41, 170);
+            this.rbTile.Name = "rbTile";
+            this.rbTile.Size = new System.Drawing.Size(42, 17);
+            this.rbTile.TabIndex = 4;
+            this.rbTile.Text = "Tile";
+            this.rbTile.UseVisualStyleBackColor = true;
+            this.rbTile.CheckedChanged += new System.EventHandler(this.rbTile_CheckedChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "man.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "man.png");
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(581, 413);
+            this.Controls.Add(this.gbView);
+            this.Controls.Add(this.listView_Students);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSearchByID);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtMajor);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Mangement System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbView.ResumeLayout(false);
+            this.gbView.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtMajor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtSearchByID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView_Students;
+        private System.Windows.Forms.ColumnHeader cID;
+        private System.Windows.Forms.ColumnHeader cName;
+        private System.Windows.Forms.ColumnHeader cAge;
+        private System.Windows.Forms.ColumnHeader aMajor;
+        private System.Windows.Forms.GroupBox gbView;
+        private System.Windows.Forms.RadioButton rbTile;
+        private System.Windows.Forms.RadioButton rbList;
+        private System.Windows.Forms.RadioButton rbSmallIcon;
+        private System.Windows.Forms.RadioButton rbLargeIcon;
+        private System.Windows.Forms.RadioButton rbDetails;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ StudentManagementSystem/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace StudentManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string StudentsFile = "students.txt";
+
+        void ClearInputFields()
+        {
+            txtStudentID.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtAge.Text = string.Empty;
+            txtMajor.Text = string.Empty;
+        }
+
+        void AddNewStudentToFile(string StudentRecord)
+        {
+            File.AppendAllText(StudentsFile, StudentRecord + Environment.NewLine);
+        }
+        void UpdateFile()
+        {
+            List <string> lines = new List<string>();
+
+            foreach (ListViewItem item in listView_Students.Items)
+            {
+                lines.Add($"{item.Text},{item.SubItems[1].Text},{item.SubItems[2].Text},{item.SubItems[3].Text}");
+            }
+            File.WriteAllLines(StudentsFile,lines);
+
+        }
+        void LoadStudentsRecordsFromFileToListView()
+        {
+            if (File.Exists(StudentsFile))
+            {
+                string[] lines = File.ReadAllLines(StudentsFile);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+
+                    ListViewItem item = new ListViewItem(parts[0]);
+                    item.SubItems.Add(parts[1]);
+                    item.SubItems.Add(parts[2]);
+                    item.SubItems.Add(parts[3]);
+                    item.ImageIndex = 0;
+
+                    listView_Students.Items.Add(item);
+                }
+            }
+        }
+
+        bool IsStudentWithThisIDExist(string StudentID)
+        {
+            foreach(ListViewItem item in listView_Students.Items)
+            {
+                if (item.Text == StudentID)
+                    return true;
+            }
+            return false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string StudentId = txtStudentID.Text;
+            string Name = txtName.Text;
+            string Age = txtAge.Text;
+            string Major = txtMajor.Text;
+            
+            if (string.IsNullOrEmpty(StudentId) || string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Age) || string.IsNullOrEmpty(Major))
+            {
+                MessageBox.Show("All Fields are Required!","Alert", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            if (IsStudentWithThisIDExist(StudentId))
+            {
+                MessageBox.Show("Student ID Already Exists!", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            //create list view item
+            ListViewItem item = new ListViewItem(StudentId);
+            item.SubItems.Add(Name);
+            item.SubItems.Add(Age);
+            item.SubItems.Add(Major);
+            item.ImageIndex = 0;
+
+            // add to listView
+            listView_Students.Items.Add(item);
+
+            // Save to file 
+            AddNewStudentToFile($"{StudentId},{Name},{Age},{Major}");
+
+            ClearInputFields();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtStudentID.Text) || string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtAge.Text) || string.IsNullOrEmpty(txtMajor.Text))
+            {
+                MessageBox.Show("All Fields are Required!", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (listView_Students.Items.Count < 0)
+            {
+                MessageBox.Show("Please Select a Student to Update", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //change the record of the selected index
+            ListViewItem SelectedItem = listView_Students.Items[0];
+            SelectedItem.SubItems[0].Text = txtStudentID.Text;
+            SelectedItem.SubItems[1].Text = txtName.Text;
+            SelectedItem.SubItems[2].Text = txtAge.Text;
+            SelectedItem.SubItems[3].Text = txtMajor.Text;
+
+            UpdateFile();
+            ClearInputFields();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView_Students.Items.Count < 0)
+            {
+                MessageBox.Show("Please Select a Student to Delete", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            listView_Students.Items.Remove(listView_Students.SelectedItems[0]);
+            UpdateFile();
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSearchByID.Text))
+            {
+                MessageBox.Show("Please Enter a Student ID to Search", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            foreach (ListViewItem item in listView_Students.Items)
+            {
+                if (item.Text == txtSearchByID.Text)
+                {
+                    string record = $"{item.SubItems[0].Text},{item.SubItems[1].Text},{item.SubItems[2].Text},{item.SubItems[3].Text}";
+                    MessageBox.Show($"Student Found!\n\nThe Record: \n[{record}]", "Student Found!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+            MessageBox.Show("Student Not Found!", "Not Found!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStudentID.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtAge.Text = string.Empty;
+            txtMajor.Text = string.Empty;
+            txtSearchByID.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load records from file when the form loads
+            LoadStudentsRecordsFromFileToListView();
+        }
+
+        private void rbDetails_CheckedChanged(object sender, EventArgs e)
+        {
+            listView_Students.View = View.Details;
+        }
+
+        private void rbLargeIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            listView_Students.View = View.LargeIcon;
+        }
+
+        private void rbSmallIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            listView_Students.View = View.SmallIcon;
+        }
+
+        private void rbList_CheckedChanged(object sender, EventArgs e)
+        {
+            listView_Students.View = View.List;
+        }
+
+        private void rbTile_CheckedChanged(object sender, EventArgs e)
+        {
+            listView_Students.View = View.Tile;
+        }
+    }
+}
